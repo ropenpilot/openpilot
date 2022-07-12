970dae19c3d75f0b1ed11005d509c7e62f1f0faf
Comma Device <device@comma.ai>
2022-07-12 16:00:31 -0400

    upload

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -35,7 +35,7 @@ procs = [
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
+  #PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
   PythonProcess("statsd", "selfdrive.statsd", persistent=True),
 
   # EON only
