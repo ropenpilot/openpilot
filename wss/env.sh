1474eb517e3d0e69f236e5a07c4f807a040cef3d
Comma Device <device@comma.ai>
2022-10-08 14:18:27 -0400

    launchipconfig

--- wss/env.sh
+++ wss/env.sh
@@ -2,4 +2,5 @@ export CARGO_HOME=/data/cargo
 export RUSTUP_HOME=/data/rustup
 
 # http bind address (addy on the ssl cert too)
-export BIND="192.168.1.10"
+export BIND="$(cat /data/openpilot/wss/cfg.c2.ip)"
+export HOST="${BIND}"
--- wss/keys.bash
+++ wss/keys.bash
@@ -7,16 +7,12 @@ source ${curdir}/env.sh
 
 keydir=${curdir}/keys
 
-curbind=${keydir}/bind_address
+curbind=${keydir}/.bind_address
 key=${keydir}/server.key.pem
 cert=${keydir}/server.cert.pem
-pfx=${keydir}/server.pfx
 
-function need_openssl { which openssl 2>/dev/null 1>&2 || \
-  (echo needopenssl; exit 1)
-}
 function already_exists {
-  test -f "${key}" -a -f "${cert}" -a -f "${pfx}" -a -f "${curbind}"
+  test -f "${key}" -a -f "${cert}" -a -f "${curbind}"
 }
 function new_bind {
   test ! "$(cat ${curbind})" = "${BIND}"
@@ -30,20 +26,13 @@ function generate_keys {
     -nodes -days 365 -newkey rsa:2048 \
     -keyout ${key} -out ${cert} -subj /CN="${BIND}"
 
-  openssl pkcs12 -export \
-    -out ${pfx} -inkey ${key} -in ${cert} \
-    -password pass:'test'
-
   cp ${cert} $(dirname ${cert})/../public
 }
-
-
-
-need_openssl
 
 if $(already_exists); then
   echo keys already exist
   if $(new_bind); then
+    # regenerate
     rm -f ${keydir}/*
   else
   exit 0
